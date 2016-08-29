0a890f3337052a2a48d837ed6f1a8e5bf67d6345
katrykat <katerismartinez@gmail.com>
2016-08-29 23:22:05 +0100

    Updating code

--- js/main.js
+++ js/main.js
@@ -3,63 +3,48 @@ $(document).ready(function() {
         url: "categories.json",
         dataType: "json",
         success: function(data) {
-        	processData(data);      	
+        	processData(data);    
         }
-     });    
+     }); 
 });
 
-//Categories
-
-function processData(Lighting) {
+function processData(data) {
 	for (var i = 0; i < data.length; i++) {
-		// Adding all the divs, images, titles, etc
-        $("#categories").append('<div id="div'+i+'"><h2 id="title'+i+'"></h2><img id="img'+i+'"/></div>');
-        // Adding titles
-        $("#title"+i).append(data[i].title);
-        // Adding img
-        $("#img"+i).attr("src",data[i].img);
-        // Adding alt attribute
-        $("#img"+i).attr("alt",data[i].title+" Section");
+                $("#categories").append('<div id="category'+i+'" class="category"><h2 id="category_title'+i+'"/></div>');
+                $("#category_title"+i).append(data[i].title);
+                for(var j = 0; j < data[i].products.length; j++){
+                        // Adding all the divs, images, titles, etc
+                        $("#category"+i).append('<div id="product'+i+j+'"><h4 id="title'+i+j+'"/><img id="img'+i+j+'"/><h5 id="price'+i+j+'"/><button id="'+i+'-'+j+'" onclick="clicked(this);" type="button">Add</button></div>');
+                        // Adding titles
+                        $("#title"+i+j).append(data[i].products[j].title);
+                        // Adding img
+                        $("#img"+i+j).attr("src",data[i].products[j].img);
+
+                        $("#price"+i+j).append(data[i].products[j].price);
+                        // Adding alt attribute
+                        // $("#img"+i).attr("alt",data[i].title+" Section");
+                        
+                }
+		
 	}
 }
-function processData(Electricity) {        
-        for (var i = 1; i < data.length; i++) {
-        $("#categories").append('<div id="div'+i+'"><h2 id="title'+i+'"></h2><img id="img'+i+'"/></div>');
-        $("#title"+i).append(data[i].title);
-        $("#img"+i).attr("src",data[i].img);
-        $("#img"+i).attr("alt",data[i].title+" Section");
-        }
-}
-function processData(Electronics) {        
-        for (var i = 2; i < data.length; i++) {
-        $("#categories").append('<div id="div'+i+'"><h2 id="title'+i+'"></h2><img id="img'+i+'"/></div>');
-        $("#title"+i).append(data[i].title);
-        $("#img"+i).attr("src",data[i].img);
-        $("#img"+i).attr("alt",data[i].title+" Section");
-        }
-}
-function processData(Connectors) {
-        for (var i = 3; i < data.length; i++) {
-        $("#categories").append('<div id="div'+i+'"><h2 id="title'+i+'"></h2><img id="img'+i+'"/></div>');
-        $("#title"+i).append(data[i].title);
-        $("#img"+i).attr("src",data[i].img);
-        $("#img"+i).attr("alt",data[i].title+" Section");
-        }
-}        
-function processData(Gardens) {
-        for (var i = 4; i < data.length; i++) {
-        $("#categories").append('<div id="div'+i+'"><h2 id="title'+i+'"></h2><img id="img'+i+'"/></div>');
-        $("#title"+i).append(data[i].title);
-        $("#img"+i).attr("src",data[i].img);
-        $("#img"+i).attr("alt",data[i].title+" Section");
-        }
-}        
-function processData(Various) {
-        for (var i = 5; i < data.length; i++) {
-        $("#categories").append('<div id="div'+i+'"><h2 id="title'+i+'"></h2><img id="img'+i+'"/></div>');
-        $("#title"+i).append(data[i].title);
-        $("#img"+i).attr("src",data[i].img);
-        $("#img"+i).attr("alt",data[i].title+" Section");
-        }
+
+// Function called every time we press a button "Add", this one extracts the id of the button and call addToCart() function
+function clicked(item) {
+        var btnId = $(item).attr("id");
+        addToCart(btnId)
 }
 
+// This function accepts an id like this "0-2" representing the element in the data to be added to the Cart
+function addToCart(id){
+        var res = id.split("-");
+        $.ajax({
+        url: "categories.json",
+        dataType: "json",
+        success: function(data) {
+                var prodTitle = data[res[0]].products[res[1]].title
+                var prodPrice = data[res[0]].products[res[1]].price
+                alert(prodTitle + " has been added to the cart with price: "+prodPrice)
+        }
+        });
+}
