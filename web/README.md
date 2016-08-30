8a231c4689c15371d7e6b28019b1439802824c07
katrykat <katerismartinez@gmail.com>
2016-08-30 19:46:49 +0100

    upload

---
+++ web/README.md
@@ -0,0 +1,11 @@
+# test_web
+web for testing
+-Instalar python 2.7 (add to Path)
+
+-Abrir CMD
+
+-cd (direccion del directorio)
+
+-python -m SimpleHTTPServer
+
+-Ve al puerto: localhost:8000 
---
+++ web/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+        //Check if cart is null
+       if (localStorage.getItem('cart') == null || localStorage.getItem('cart') == ""){
+                localStorage.setItem('cart', '[]');
+        }
+	$.ajax({
+        url: "categories.json",
+        dataType: "json",
+        success: function(data) {
+        	processData(data);    
+        }
+     });
+     updateCartIcon(); 
+});
+
+function updateCartIcon(){
+        var cart = JSON.parse(localStorage.getItem('cart'));
+        var total = 0;
+        for (var i = 0; i < cart.length; i++) {
+                // if (cart[i]!="del") {
+                      total++;  
+                // }   
+        }
+        $("#cart-count").attr("value", total);
+}
+
+function processData(data) {
+	for (var i = 0; i < data.length; i++) {
+                $("#categories").append('<div id="category'+i+'" class="category"><h2 id="category_title'+i+'"/></div>');
+                $("#category_title"+i).append(data[i].title);
+                for(var j = 0; j < data[i].products.length; j++){
+                        // Adding all the divs, images, titles, etc
+                        $("#category"+i).append('<div id="product'+i+j+'"><h4 id="title'+i+j+'"/><img id="img'+i+j+'"/><h5 id="price'+i+j+'"/><button id="'+i+'-'+j+'" onclick="clicked(this);" type="button">Add</button></div>');
+                        // Adding titles
+                        $("#title"+i+j).append(data[i].products[j].title);
+                        // Adding img
+                        $("#img"+i+j).attr("src",data[i].products[j].img);
+
+                        $("#price"+i+j).append(data[i].products[j].price);
+                        // Adding alt attribute
+                        // $("#img"+i).attr("alt",data[i].title+" Section");
+                        
+                }
+		
+	}
+}
+
+// Function called every time we press a button "Add", this one extracts the id of the button and call addToCart() function
+function clicked(item) {
+        var btnId = $(item).attr("id");
+        addToCart(btnId)
+}
+
+// This function accepts an id like this "0-2" representing the element in the data to be added to the Cart
+function addToCart(id){
+        var res = id.split("-");
+        $.ajax({
+        url: "categories.json",
+        dataType: "json",
+        success: function(data) {
+                var prodTitle = data[res[0]].products[res[1]].title
+                var prodPrice = data[res[0]].products[res[1]].price
+                // alert(prodTitle + " has been added to the cart with price: "+prodPrice)
+                var cart = JSON.parse(localStorage.getItem('cart'));
+                cart.push(data[res[0]].products[res[1]]);
+                localStorage.setItem('cart',JSON.stringify(cart));
+                updateCartIcon();
+        }
+        });
+}
